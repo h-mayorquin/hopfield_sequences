fc0590f6393b5db7e2616ff7f4f220be9f77c7de
h-mayorquin <h.mayorquin@gmail.com>
2016-06-01 17:11:44 +0200

    Added renormalization

--- hopfield.py
+++ hopfield.py
@@ -13,6 +13,9 @@ class Hopfield():
 
         self.state = s
         self.n_dim = s.shape[0]
+        self.list_of_patterns = None
+        self.w = None
+        self.state_distance = np.ones(self.n_dim)
 
     def train(self, list_of_patterns):
         """
@@ -22,11 +25,13 @@ class Hopfield():
         :return: w  the weight matrix.
         """
 
+        self.list_of_patterns = list_of_patterns
         self.w = np.zeros((self.n_dim, self.n_dim))
+
         for pattern in list_of_patterns:
             self.w += np.outer(pattern, pattern)
 
-        self.w *= (1.0 / self.n_dim)
+        # self.w *= (1.0 / self.n_dim)
 
         return self.w
 
@@ -36,6 +41,30 @@ class Hopfield():
         :return: next state
         """
 
-        self.state = np.dot(self.w, self.state) > 0
-        return self.state * 1.0
+        self.state = np.dot(self.w, self.state)
+        return self.state
+
+    def normalize(self):
+        """
+        Normalizes the state
+        :return: the normalize state
+        """
+
+        self.state = self.state / np.sum(self.state)
+
+        return self.state
+
+    def calculate_state_distance(self):
+        """
+        Calcualtes the distance between the state and
+        all the patterns
+
+        :return: A state distance vector with the distance between
+         the actual state of the system and all the stored patterns
+        """
+
+        for index, pattern in enumerate(self.list_of_patterns):
+            self.state_distance[index] = np.linalg.norm(self.state - pattern)
+
+        return self.state_distance
 
--- play.py
+++ play.py
@@ -2,16 +2,24 @@ import numpy as np
 from hopfield import Hopfield
 
 
-s0 = np.array([1, 0, 0])
-s1 = np.array([0, 1, 0])
+s0 = np.array([1, 0])
+s1 = np.array([0, 1])
 
-s = np.array([0.5, 0.3, 8])
+s = np.array([0.9, 0.1])
 
 network = Hopfield(s)
 
 list_of_patterns = [s0, s1]
 w = network.train(list_of_patterns)
 
-N = 3
+print(w)
+print(network.w)
+print('Running')
+
+N = 5
 for i in range(N):
-    print(network.update())
+    print('state', network.state)
+    print('update', network.update())
+    print('normalize', network.normalize())
+    print('state distance', network.calculate_state_distance())
+
