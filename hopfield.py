0fd4c21e6facc0b47e4fd928cfc6bbc576ad6c5b
h-mayorquin <h.mayorquin@gmail.com>
2016-12-13 22:40:18 +0100

    Added hopfield sequence

--- hopfield.py
+++ hopfield.py
@@ -1,4 +1,5 @@
 import numpy as np
+import collections
 
 
 class Hopfield():
@@ -105,3 +106,133 @@ class Hopfield():
 
         return self.state_distance
 
+
+class HopfieldSequence():
+    """
+    The hopfield as a sequence
+    """
+
+    def __init__(self, n_dim=3, tau=10, g_delay=1.0, T=0, prng=np.random):
+        self.prng = prng
+
+        self.n_dim = n_dim
+        self.tau = tau
+        self.g_delay = g_delay
+        self.s = np.sign(prng.normal(size=n_dim))
+        self.h = np.zeros(n_dim)
+
+        # Noise parameters
+        self.T = T
+        self.sigma = T / (2 * np.sqrt(2))  # Check page 67 of Amit to see where does this comes from
+
+        self.list_of_patterns = None
+        self.w = None
+        self.w_delay = None
+        self.m = None
+        self.state_distance = None
+
+        aux = [np.zeros(n_dim) for i in range(self.tau)]
+        self.s_history = collections.deque(aux, maxlen=self.tau)
+
+    def train(self, list_of_patterns, normalize=True):
+        """
+        Implements the Hebbian learning rule
+
+        :param list_of_patterns: This is a list with the desired parameters for equilibrum
+        normalize: normalizes the w matrix by its dimension
+        :return: w  the weight matrix.
+        """
+
+        self.list_of_patterns = list_of_patterns
+        self.w = np.zeros((self.n_dim, self.n_dim))
+
+        for pattern in list_of_patterns:
+            self.w += np.outer(pattern, pattern)
+
+        if normalize:
+            self.w *= (1.0 / self.n_dim)
+
+        # zeros in the diagonal
+        self.w[np.diag_indices_from(self.w)] = 0
+
+    def train_delays(self, list_of_patterns, normalize=True):
+
+        self.list_of_patterns_sequence = list_of_patterns
+        self.w_delay = np.zeros((self.n_dim, self.n_dim))
+
+        for pattern in list_of_patterns:
+            self.w_delay += np.outer(pattern, pattern)
+
+        if normalize:
+            self.w_delay *= (1.0 / self.n_dim)
+
+        # zeros in the diagonal
+        self.w_delay[np.diag_indices_from(self.w_delay)] = 0
+
+    def generate_random_patterns(self, n_store):
+        list_of_patterns = [np.sign(self.prng.normal(size=self.n_dim)) for i in range(n_store)]
+
+        return list_of_patterns
+
+    def update_sync(self):
+        """
+        Updates the network state of all the neurons at the same time
+        """
+
+        if self.sigma < 0.001:
+            noise = 0
+        else:
+            noise = self.prng.normal(0, scale=self.sigma, size=self.n_dim)
+
+
+        # Linear part
+        self.h = np.dot(self.w, self.s) + \
+                 self.g_delay * np.dot(self.w_delay, self.s_history.pop()) + noise
+
+        # Non-linear part
+        # self.state = sigmoid_logistic(self.state)
+        self.s = np.sign(self.h)
+        self.s_history.appendleft(self.s)
+
+
+    def update_async(self):
+        """
+        Updates the network state one neuron at a time
+        """
+        # Generate random number
+        i = self.prng.randint(self.n_dim, size=1)[0]
+        # Linear
+        # self.state = np.dot(self.state, self.w[i, ...])
+
+        if self.sigma < 0.001:
+            noise = 0
+        else:
+            noise = self.prng.normal(loc=self.sigma)
+
+        self.h[i] = np.dot(self.w[i, ...], self.s) \
+                    + self.g_delay * np.dot(self.w_delay[i, ...], self.s_history.pop()) + noise
+        # Non-linear
+        self.s[i] = np.sign(self.h[i])
+        self.s_history.appendleft(self.s)
+
+    def calculate_overlap(self):
+        self.m = np.mean(self.s * self.list_of_patterns, axis=1)
+
+        return self.m
+
+
+    def calculate_state_distance(self):
+        """
+        Calcualtes the distance between the state and
+        all the patterns
+
+        :return: A state distance vector with the distance between
+         the actual state of the system and all the stored patterns
+        """
+
+        self.state_distance = np.ones(len(self.list_of_patterns))
+
+        for index, pattern in enumerate(self.list_of_patterns):
+            self.state_distance[index] = np.linalg.norm(self.s - pattern)
+
+        return self.state_distance
--- play.py
+++ play.py
@@ -2,18 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from hopfield import Hopfield
+from hopfield import Hopfield, HopfieldSequence
 
-n_dim = 5
-n_store = 2
-p_1 = np.array((1, -1, 1, 1, -1))
-p_2 = np.array((1, 1, 1, -1, -1))
+prng = np.random.RandomState(seed=100)
 
-list_of_patterns = [p_1, p_2]
-
-nn = Hopfield(n_dim=n_dim)
-nn.train(list_of_patterns, normalize=False)
-
-print(nn.w)
+n_dim = 10
+n_store = 5
+T = 0.0
+tau = 10
 
 
+if True:
+    list_of_patterns = [np.sign(np.random.normal(size=n_dim)) for i in range(n_store)]
+else:
+    list_of_patterns = [-1 * np.ones(n_dim) for i in range(n_store)]
+    for i in range(n_store):
+        list_of_patterns[i][i] = 1
+
+list_of_patterns_sequence = list_of_patterns[:3]
+
+nn = HopfieldSequence(n_dim=n_dim, tau=tau, g_delay=2.0, T=T, prng=prng)
+nn.train(list_of_patterns, normalize=True)
+nn.train_delays(list_of_patterns_sequence, normalize=True)
+
+N = 20
+
+nn.s = np.copy(list_of_patterns[0])
+history = np.zeros((N, n_store))
+for i in range(N):
+    nn.update_async()
+    history[i, :] = nn.calculate_overlap()
+
+
+# Plot
+if True:
+    fig = plt.figure(figsize=(16, 12))
+    ax = fig.add_subplot(111)
+
+    for index, overlap in enumerate(history.T):
+        ax.plot(overlap, '-*', label=str(index))
+
+    ax.set_ylim([-1.2, 1.2])
+    ax.legend()
+    plt.show()
