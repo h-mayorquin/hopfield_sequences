a046418768279d9441e73c76dd7262d2e8213195
h.mayorquin <h.mayorquin@gmail.com>
2016-12-14 02:22:36 +0100

    Added differentaible Hopfield

--- hopfield.py
+++ hopfield.py
@@ -248,6 +248,88 @@ class HopfieldSequence():
 
         return self.m
 
+
+def calculate_state_distance(self):
+    """
+    Calcualtes the distance between the state and
+    all the patterns
+
+    :return: A state distance vector with the distance between
+     the actual state of the system and all the stored patterns
+    """
+
+    self.state_distance = np.ones(len(self.list_of_patterns))
+
+    for index, pattern in enumerate(self.list_of_patterns):
+        self.state_distance[index] = np.linalg.norm(self.s - pattern)
+
+    return self.state_distance
+
+
+class HopfieldDiff():
+
+    def __init__(self, n_dim=3, tau_m=20.0, dt=0.1, T=0, prng=np.random):
+
+        self.prng = prng
+
+        self.tau_m = tau_m
+        self.dt = dt
+
+        self.n_dim = n_dim
+        self.s = np.sign(prng.normal(size=n_dim))
+        self.h = np.zeros(n_dim)
+
+        # Noise parameters
+        self.T = T
+        self.sigma = T / (2 * np.sqrt(2))  # Check page 67 of Amit to see where does this comes from
+
+        self.list_of_patterns = None
+        self.w = None
+        self.m = None
+        self.state_distance = None
+
+    def train(self, list_of_patterns, normalize=True):
+        """
+        Implements the Hebbian learning rule
+
+        :param list_of_patterns: This is a list with the desired parameters for equilibrum
+        normalize: normalizes the w matrix by its dimension
+        :return: w  the weight matrix.
+        """
+
+        self.list_of_patterns = list_of_patterns
+        self.w = np.zeros((self.n_dim, self.n_dim))
+
+        for pattern in list_of_patterns:
+            self.w += np.outer(pattern, pattern)
+
+        if normalize:
+            self.w *= (1.0 / self.n_dim)
+
+        # zeros in the diagonal
+        self.w[np.diag_indices_from(self.w)] = 0
+
+    def generate_random_patterns(self, n_store):
+
+        list_of_patterns = [np.sign(self.prng.normal(size=self.n_dim)) for i in range(n_store)]
+
+        return list_of_patterns
+
+    def update(self):
+        """
+        Updates the network state of all the neurons at the same time
+        """
+
+        if self.sigma < 0.001:
+            noise = 0
+        else:
+            noise = self.prng.normal(0, scale=self.sigma, size=self.n_dim)
+
+        aux = np.dot(self.w, np.tanh(self.s))
+        aux = np.ones(self.n_dim) * 2
+        self.s += (self.dt / self.tau_m) * (aux - self.s + noise)
+
+
     def calculate_state_distance(self):
         """
         Calcualtes the distance between the state and
--- play.py
+++ play.py
@@ -2,48 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from hopfield import Hopfield, HopfieldSequence
+from hopfield import Hopfield, HopfieldSequence, HopfieldDiff
 
 prng = np.random.RandomState(seed=101)
 
-n_dim = 200
-n_store = 10
-q = 10
+n_dim = 10
+n_store = 3
 T = 0.0
-tau = 5
+
+dt = 0.1
+tau_m = 20
 
 
-if True:
-    list_of_patterns = [np.sign(np.random.normal(size=n_dim)) for i in range(n_store)]
-else:
-    list_of_patterns = [-1 * np.ones(n_dim) for i in range(n_store)]
-    for i in range(n_store):
-        list_of_patterns[i][i] = 1
-
-list_of_patterns_sequence = list_of_patterns[:q]
-
-nn = HopfieldSequence(n_dim=n_dim, tau=tau, g_delay=2.0, T=T, prng=prng)
+nn = HopfieldDiff(n_dim=n_dim, tau_m=tau_m, dt=dt, T=T, prng=prng)
+list_of_patterns = nn.generate_random_patterns(n_store)
 nn.train(list_of_patterns, normalize=True)
-nn.train_delays(list_of_patterns_sequence, normalize=True)
 
-N = 70
+N = 1000
+history = np.zeros((N, n_dim))
 
-nn.s = np.copy(list_of_patterns[0])
-history = np.zeros((N, n_store))
 for i in range(N):
-    # nn.update_sync()
-    nn.update_async_random_sequence()
-    # nn.update_async()
-    history[i, :] = nn.calculate_overlap()
+    nn.update()
+    history[i, ...] = nn.s
 
-# Plot
-if True:
-    fig = plt.figure(figsize=(16, 12))
-    ax = fig.add_subplot(111)
+fig = plt.figure(figsize=(16, 12))
+ax = fig.add_subplot(111)
 
-    for index, overlap in enumerate(history.T):
-        ax.plot(overlap, '-*', label=str(index))
+for s in history.T:
+    ax.plot(s)
 
-    ax.set_ylim([-1.2, 1.2])
-    ax.legend()
-    plt.show()
+plt.show()
